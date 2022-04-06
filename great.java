7cc325180d7d8abdad21c40d2ea6091ab89436de
Piyush <87450143+PiyushYadavPY@users.noreply.github.com>
2022-04-06 18:26:04 +0530

    Add files via upload

---
+++ great.java
@@ -0,0 +1,27 @@
+package greatest;
+
+public class great{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=40,b=50,c=60;
+       if(a>b)
+       {
+    	   if(a>c)
+    		   System.out.print(a + " is greatest");
+    	   else
+    	       System.out.print(c + " is greatest");
+    	   
+       }
+       else
+       {
+    	   if(b>c)
+    		   System.out.print(b + " is greatest");
+    	   else
+    	       System.out.print(c + " is greatest"); 
+        		   
+        	 
+       }
+	}
+
+}
